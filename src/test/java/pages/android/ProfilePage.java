5ad859f5097026296e159724c0cd00a95163bc6c
Sunny Goel <mac@macs-MacBook-Pro.local>
2018-04-24 16:14:56 +0530

    Added New Test Cases and minor code enhancements -> Test Cases for Profile Name Field -> Minor Variable name enhancements suggested in last PR

--- src/test/java/pages/android/ProfilePage.java
+++ src/test/java/pages/android/ProfilePage.java
@@ -1,9 +1,12 @@
 package pages.android;
 
 import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileDriver;
+import io.appium.java_client.TouchAction;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.touch.TouchActions;
 
 public class ProfilePage extends BasePage {
 
@@ -13,25 +16,27 @@ public class ProfilePage extends BasePage {
 
 
 	By edit_button = By.id("action_edit_profile");
-	By gender_button = By.id("app.goplus.in.myapplication.qa:id/edit_profile_gender");
-	By nameField = By.id("app.goplus.in.myapplication.qa:id/profile_name");
+	By gender_button = By.id("edit_profile_gender");
+	By nameField = By.id("profile_name");
 	By mobileNumberField = By.xpath("//android.widget.TextView[contains(@resource-id,'profile_number')]");
-	By birthdayField = By.id("app.goplus.in.myapplication.qa:id/edit_profile_birthday");
-	By emailField = By.id("app.goplus.in.myapplication.qa:id/edit_profile_email");
-	By homeAddressField = By.id("app.goplus.in.myapplication.qa:id/profile_home_edit_text");
-	By homeLeaveTime = By.id("app.goplus.in.myapplication.qa:id/profile_home_time_user");
-	By officeAddress = By.id("app.goplus.in.myapplication.qa:id/profile_office_text");
-	By officeLeaveTime = By.id("app.goplus.in.myapplication.qa:id/profile_office_edit_time");
-	By corporateEmail = By.id("app.goplus.in.myapplication.qa:id/profile_connected_account_tv");
-	By profileImageField = By.id("app.goplus.in.myapplication.qa:id/profile_picture");
+	By birthdayField = By.id("edit_profile_birthday");
+	By emailField = By.id("edit_profile_email");
+	By homeAddressField = By.id("profile_home_edit_text");
+	By homeLeaveTime = By.id("profile_home_time_user");
+	By officeAddress = By.id("profile_office_text");
+	By officeLeaveTime = By.id("profile_office_edit_time");
+	By corporateEmail = By.id("profile_connected_account_tv");
+	By profileImageField = By.id("profile_picture");
 
 
 
 	public String getProfileImage(){
 		waitForVisibilityOf(profileImageField);
 		String profileSRC =  driver.findElement(profileImageField).getText();
-		scrollDown();
-		return profileSRC;
+		WebElement element = driver.findElement(gender_button);
+        scrollNewMethod(element);
+
+        return profileSRC;
 	}
 
 	public String getGender() {
@@ -125,6 +130,44 @@ public class ProfilePage extends BasePage {
 				".scrollIntoView(" + neededElement_finder + ")"));
 		System.out.println("I am here");
 	}
+
+
+	public void scrollDownAgain()
+    {
+
+        System.out.print("In ScrollAgain Method");
+        WebElement profileElement = driver.findElement(profileImageField);
+        WebElement email = driver.findElement(birthdayField);
+        TouchAction touchAction = new TouchAction((MobileDriver) driver);
+        touchAction.longPress(profileElement);
+        touchAction.moveTo(email);
+        touchAction.release();
+
+        System.out.print("Leaving Method");
+    }
+
+
+    public void scrollNewMethod(WebElement element)
+    {
+
+            System.out.println("In ScrollNew Method");
+
+
+            TouchActions action = new TouchActions(driver);
+            action.scroll(element, 0, 200);
+
+            System.out.println("Leaving Method");
+
+    }
+
+    public void getLocationOfElement(WebElement element)
+    {
+
+        System.out.println("X Location  : " + element.getLocation().getX());
+        System.out.println("Y Location  : " + element.getLocation().getY());
+
+        System.out.println("Height  : " + element.getSize());
+    }
 
 
 }
--- src/test/java/tests/android/ProfileTest.java
+++ src/test/java/tests/android/ProfileTest.java
@@ -1,8 +1,10 @@
 package tests.android;
 
+import io.appium.java_client.android.AndroidDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.android.*;
 import common.android.*;
@@ -13,6 +15,9 @@ public class ProfileTest extends Setup {
     private Commons common;
     private MenuPage menuPage;
     private ProfilePage profilePage;
+    private PersonalDetailsPage personalDetailsPage ;
+
+    private AndroidDriver androidDriver ;
 
     @BeforeClass
     public void setUp() {
@@ -20,15 +25,16 @@ public class ProfileTest extends Setup {
         try
         {
             createAndroidSession(true);
+            androidDriver = (AndroidDriver) driver ;
 
             common = new Commons(driver);
-            common.enterUserPhoneNumberOTP("newUserPhoneNumber", "OTP");
-            common.enterPersonalDetailsNewUser();
-            common.enterHomeAddressDetailsNewUser();
-            common.enterOfficeAddressDetailsNewUser();
+//            common.enterUserPhoneNumberOTP("newUserPhoneNumber", "OTP");
+//            common.enterPersonalDetailsNewUser();
+//            common.enterHomeAddressDetailsNewUser();
+//            common.enterOfficeAddressDetailsNewUser();
 
-            common.goToHomepage();
-            Thread.sleep(2000);
+            //common.goToHomepage();
+
 
 
             homePage = new HomePage(driver);
@@ -38,6 +44,9 @@ public class ProfileTest extends Setup {
             menuPage.clickProfile();
 
             profilePage = new ProfilePage(driver);
+            personalDetailsPage = new PersonalDetailsPage(driver);
+
+
         }
         catch(Exception e )
         {
@@ -49,94 +58,263 @@ public class ProfileTest extends Setup {
 
     @Test(priority = 1 , groups = {"New User"})
     public void verifyNewUserDefaultNumber() throws Exception {
-        String Text = profilePage.getNumber();
-        Assert.assertEquals(Text, "+91"+getValueFromPPFile("newUserPhoneNumber"));
+        String defaultNumber = profilePage.getNumber();
+        Assert.assertEquals(defaultNumber, "+91"+getValueFromPPFile("newUserPhoneNumber"));
     }
+
 
     @Test(priority = 2 , groups = {"New User"})
     public void verifyNewUserDefaultName() throws Exception {
-        String Text = profilePage.getName();
-        Assert.assertEquals(Text, getValueFromPPFile("userName"));
+        String defaultName = profilePage.getName();
+        Assert.assertEquals(defaultName, getValueFromPPFile("userName"));
     }
-
 
 
     @Test(priority = 3 , groups = {"New User"})
     public void verifyNewUserDefaultProfilePicture() {
-        String Text = profilePage.getProfileImage();
-        System.out.println("Profile SRC : " + Text);
-        Assert.assertEquals(Text, "Profile SRC");
+        String defaultProfileSRC = profilePage.getProfileImage();
+        System.out.println("Profile SRC : " + defaultProfileSRC);
+        Assert.assertEquals(defaultProfileSRC, "");
     }
 
 
-
     @Test(priority = 4 , groups = {"New User"})
-    public void verifyNewUserDefaultBirthday(){
-        String Text = profilePage.getBirthday();
-        System.out.println("Birthday : " + Text);
-        Assert.assertEquals(Text, "");
+    public void verifyNewUserDefaultGender() throws Exception {
+        String defaultGender = profilePage.getGender();
+        if (getValueFromPPFile("gender").equalsIgnoreCase("F"))
+            Assert.assertEquals(defaultGender, "Female");
+        else if (getValueFromPPFile("gender").equalsIgnoreCase("M"))
+            Assert.assertEquals(defaultGender, "Male");
     }
 
 
     @Test(priority = 5 , groups = {"New User"})
-    public void verifyNewUserDefaultGender() throws Exception {
-        String Text = profilePage.getGender();
-        System.out.println("Gender : " + Text);
-
-        if (getValueFromPPFile("gender").equalsIgnoreCase("F"))
-            Assert.assertEquals(Text, "Female");
-        else if (getValueFromPPFile("gender").equalsIgnoreCase("M"))
-            Assert.assertEquals(Text, "Male");
+    public void verifyNewUserDefaultEmail(){
+        String defaultEmail = profilePage.getEmail();
+        Assert.assertEquals(defaultEmail, "");
     }
 
 
     @Test(priority = 6 , groups = {"New User"})
-    public void verifyNewUserDefaultEmail(){
-        String Text = profilePage.getEmail();
-        System.out.println("Email : " + Text);
-        Assert.assertEquals(Text, "");
+    public void verifyNewUserDefaultBirthday(){
+        androidDriver.scrollTo("Birthday");
+        String defaultBirthDate = profilePage.getBirthday();
+        Assert.assertEquals(defaultBirthDate, "");
     }
 
 
     @Test(priority = 7 , groups = {"New User"})
     public void verifyNewUserDefaultHomeAddress(){
-        String Text = profilePage.getHomeAddress();
-        System.out.println("Home Address : " + Text);
-        Assert.assertEquals(Text, "Home Address");
+        androidDriver.scrollTo("Home Address");
+        String defaultHomeAddress = profilePage.getHomeAddress();
+        System.out.println("Home Address : " + defaultHomeAddress);
+        Assert.assertEquals(defaultHomeAddress, "");
     }
 
 
     @Test(priority = 8 , groups = {"New User"})
     public void verifyNewUserDefaultHomeLeaveTime(){
-        String Text = profilePage.getHomeLeaveTime();
-        System.out.println("Home Leave Time : " + Text);
-        Assert.assertEquals(Text, "Home Leave Time");
+        String defaultHomeLeaveTime = profilePage.getHomeLeaveTime();
+        System.out.println("Home Leave Time : " + defaultHomeLeaveTime);
+        Assert.assertEquals(defaultHomeLeaveTime, "");
     }
 
 
     @Test(priority = 9 , groups = {"New User"})
     public void verifyNewUserDefaultOfficeAddress() {
-        String Text = profilePage.getOfficeAddress();
-        System.out.println("Office Address : " + Text);
-        Assert.assertEquals(Text, "Office Address");
+        androidDriver.scrollTo("Office Address");
+        String defaulOfficeAddress = profilePage.getOfficeAddress();
+        System.out.println("Office Address : " + defaulOfficeAddress);
+        Assert.assertEquals(defaulOfficeAddress, "Office Address");
     }
 
 
     @Test(priority = 10 , groups = {"New User"})
     public void verifyNewUserDefaulOfficeLeaveTime() {
-        String Text = profilePage.getOfficeLeaveTime();
-        System.out.println("Office Leave Time  : " + Text);
-        Assert.assertEquals(Text, "Office Leave Time ");
+        String defaultOfficeLeaveTime = profilePage.getOfficeLeaveTime();
+        System.out.println("Office Leave Time  : " + defaultOfficeLeaveTime);
+        Assert.assertEquals(defaultOfficeLeaveTime, "");
     }
 
 
     @Test(priority = 11 , groups = {"New User"})
     public void verifyNewUserDefaultCorporateEmail(){
-        String Text = profilePage.getCorporateEmail();
-        System.out.println("Corporate Email : " + Text);
-        Assert.assertEquals(Text, "Corporate Email");
+        androidDriver.scrollTo("CORPORATE ACCOUNT");
+        String defaultCorporateEmail = profilePage.getCorporateEmail();
+        System.out.println("Corporate Email : " + defaultCorporateEmail);
+        Assert.assertEquals(defaultCorporateEmail, "");
     }
 
+
+
+    // -----------------------    CHECK FIELD VALIDATIONS     ---------------------------
+
+
+    @Test(priority = 1 , groups = {"Edit User"} , dataProvider = "enterDifferentName")
+    public void verifyNameValidation(String name , String expected){
+        personalDetailsPage.enterUserName(name);
+        System.out.println("Error Message : " + defaultCorporateEmail);
+        Assert.assertEquals(defaultCorporateEmail, "");
+    }
+
+
+    @DataProvider
+    public Object[][] enterDifferentName()
+    {
+        Object [][] nameError = new String[5][2];
+
+        nameError[0][0] = "   ";
+        nameError[0][1] = "";
+
+        nameError[1][0] = ";'[]";
+        nameError[1][1] = "";
+
+        nameError[2][0] = "a";
+        nameError[2][1] = "";
+
+        nameError[3][0] = "     Test Spaces";
+        nameError[3][1] = "";
+
+        nameError[4][0] = "Correct Name";
+        nameError[4][1] = "";
+
+        return nameError;
+    }
+
+
+
+    @Test(priority = 2 , groups = {"Edit User"} , dataProvider = "enterDifferentEmail")
+    public void verifyPersonalEmailValidation(String personalEmail , String expected){
+        personalDetailsPage.enterUserName(personalEmail);
+        System.out.println("Error Message : " + defaultCorporateEmail);
+        Assert.assertEquals(defaultCorporateEmail, "");
+    }
+
+
+    @DataProvider
+    public Object[][] enterDifferentEmail()
+    {
+        Object [][] emailError = new String[5][2];
+
+        emailError[0][0] = "   ";
+        emailError[0][1] = "";
+
+        emailError[1][0] = "  @  .com";
+        emailError[1][1] = "";
+
+        emailError[2][0] = "ugiguiihuk";
+        emailError[2][1] = "";
+
+        emailError[3][0] = ".@.";
+        emailError[3][1] = "";
+
+        emailError[4][0] = "test@shuttl.com";
+        emailError[4][1] = "";
+
+        return emailError;
+    }
+
+    @Test(priority = 11 , groups = {"Edit User"} , dataProvider = "enterDifferentEmail")
+    public void verifyCorporateEmailValidation(String corporateEmail , String expected){
+        personalDetailsPage.enterUserName(corporateEmail);
+        System.out.println("Error Message : " + defaultCorporateEmail);
+        Assert.assertEquals(defaultCorporateEmail, "");
+    }
+
+
+
+    // -----------------------    CHECK EACH FIELD FOR EXISTING USER    -----------------------
+
+
+
+    @Test(priority = 1 , groups = {"Existing User"})
+    public void verifyExistingUserDefaultNumber() throws Exception {
+        String defaultNumber = profilePage.getNumber();
+        Assert.assertEquals(defaultNumber, "+91"+getValueFromPPFile("newUserPhoneNumber"));
+    }
+
+
+    @Test(priority = 2 , groups = {"Existing User"})
+    public void verifyExistingUserDefaultName() throws Exception {
+        String defaultName = profilePage.getName();
+        Assert.assertEquals(defaultName, getValueFromPPFile("userName"));
+    }
+
+
+    @Test(priority = 3 , groups = {"Existing User"})
+    public void verifyExistingUserDefaultProfilePicture() {
+        String defaultProfileSRC = profilePage.getProfileImage();
+        System.out.println("Profile SRC : " + defaultProfileSRC);
+        Assert.assertEquals(defaultProfileSRC, "");
+    }
+
+
+    @Test(priority = 4 , groups = {"Existing User"})
+    public void verifyExistingUserDefaultGender() throws Exception {
+        String defaultGender = profilePage.getGender();
+        if (getValueFromPPFile("gender").equalsIgnoreCase("F"))
+            Assert.assertEquals(defaultGender, "Female");
+        else if (getValueFromPPFile("gender").equalsIgnoreCase("M"))
+            Assert.assertEquals(defaultGender, "Male");
+    }
+
+
+    @Test(priority = 5 , groups = {"Existing User"})
+    public void verifyExistingUserDefaultEmail(){
+        String defaultEmail = profilePage.getEmail();
+        Assert.assertEquals(defaultEmail, "");
+    }
+
+
+    @Test(priority = 6 , groups = {"Existing User"})
+    public void verifyExistingUserDefaultBirthday(){
+        androidDriver.scrollTo("Birthday");
+        String defaultBirthDate = profilePage.getBirthday();
+        Assert.assertEquals(defaultBirthDate, "");
+    }
+
+
+    @Test(priority = 7 , groups = {"Existing User"})
+    public void verifyExistingUserDefaultHomeAddress(){
+        androidDriver.scrollTo("Home Address");
+        String defaultHomeAddress = profilePage.getHomeAddress();
+        System.out.println("Home Address : " + defaultHomeAddress);
+        Assert.assertEquals(defaultHomeAddress, "");
+    }
+
+
+    @Test(priority = 8 , groups = {"Existing User"})
+    public void verifyExistingUserDefaultHomeLeaveTime(){
+        String defaultHomeLeaveTime = profilePage.getHomeLeaveTime();
+        System.out.println("Home Leave Time : " + defaultHomeLeaveTime);
+        Assert.assertEquals(defaultHomeLeaveTime, "");
+    }
+
+
+    @Test(priority = 9 , groups = {"Existing User"})
+    public void verifyExistingUserDefaultOfficeAddress() {
+        androidDriver.scrollTo("Office Address");
+        String defaulOfficeAddress = profilePage.getOfficeAddress();
+        System.out.println("Office Address : " + defaulOfficeAddress);
+        Assert.assertEquals(defaulOfficeAddress, "Office Address");
+    }
+
+
+    @Test(priority = 10 , groups = {"Existing User"})
+    public void verifyExistingUserDefaulOfficeLeaveTime() {
+        String defaultOfficeLeaveTime = profilePage.getOfficeLeaveTime();
+        System.out.println("Office Leave Time  : " + defaultOfficeLeaveTime);
+        Assert.assertEquals(defaultOfficeLeaveTime, "");
+    }
+
+
+    @Test(priority = 11 , groups = {"Existing User"})
+    public void verifyExistingUserDefaultCorporateEmail(){
+        androidDriver.scrollTo("CORPORATE ACCOUNT");
+        String defaultCorporateEmail = profilePage.getCorporateEmail();
+        System.out.println("Corporate Email : " + defaultCorporateEmail);
+        Assert.assertEquals(defaultCorporateEmail, "");
+    }
 
 
     @AfterClass
