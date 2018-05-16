65b6dce7320054aa15f7b2eb98a7ca2a61af6427
Sunny Goel <mac@macs-MacBook-Pro.local>
2018-05-16 19:00:38 +0530

    Completed App Tour in iOS —> Added generic functions of swipe (all directions) —> functions for creating a screenshot —> functions for finding image percentage difference pixel by pixel —> Test Cases for app tour —> Created directory of default app tour pages images

--- src/test/java/common/ios/Constants.java
+++ src/test/java/common/ios/Constants.java
@@ -3,5 +3,12 @@ package common.ios;
 public class Constants
 {
 
-    public static String finalProfileDataTextFile = System.getProperty("user.dir")+"/src/Resources/FinalProfileData.txt";
+    public static String appTourExpectedScreenshots = "/Users/mac/uiautomation/src/Resources" +
+            "/AppTourPageScreenShots/Expected View/";
+
+    public static String appTourErrorScreenshots = "/Users/mac/uiautomation/src/Resources" +
+            "/AppTourPageScreenShots/Actual View";
+
+    public static String finalProfileDataTextFile = System.getProperty("user.dir")+"/src/Resources" +
+            "/FinalProfileData.txt";
 }
