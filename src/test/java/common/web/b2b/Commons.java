231ea847ec6e916751eebb5e6f76e7fc60bc765a
nitish-bhardwaj-code <nitish@MacBook-Pro.local>
2018-05-23 18:51:49 +0530

    renaming file

--- src/test/java/common/web/b2b/Commons.java
+++ src/test/java/common/web/b2b/Commons.java
@@ -2,11 +2,13 @@ package common.web.b2b;
 
 import org.openqa.selenium.WebDriver;
 import pages.android.BasePage;
+import pages.web.b2b.LoginPanelPage;
 
-public class commons extends BasePage {
-    public commons(WebDriver driver) {
+public class Commons extends BasePage {
+    public Commons(WebDriver driver) {
         super(driver);
     }
+    private LoginPanelPage loginPanelPage = new LoginPanelPage(driver);
 
 
     public void login() throws InterruptedException {
