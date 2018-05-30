48e6ace93cd4609db3cbc94fe50a1d60445a7ffe
arpit <arpit.gandhi@shuttl.com>
2018-05-30 17:32:49 +0530

    Resolve namingConvention for MyRidesTest.java

--- src/test/java/tests/MyRidesTest.java
+++ src/test/java/tests/MyRidesTest.java
@@ -25,58 +25,58 @@ public class MyRidesTest extends Setup {
     }
 
     @Test(priority = 1)
-    public void myRidesDisplayText() throws Exception {
+    public void verifyMyRidesDisplayText() throws Exception {
         String ridesDisplayText = myRidesPage.getMyRidesDisplayText();
         Assert.assertEquals(ridesDisplayText, "My Rides");
         myRidesPage.getMyRidesDisplayTextClick();
     }
 
-    @Test(priority = 2, dependsOnMethods = "myRidesDisplayText")
-    public void myRidesTitleText() throws Exception {
+    @Test(priority = 2, dependsOnMethods = "verifyMyRidesDisplayText")
+    public void verifyMyRidesTitleText() throws Exception {
 
         String myRidesTitleText = myRidesPage.getMyRidesTitleText();
         Assert.assertEquals(myRidesTitleText, "My Rides");
 
     }
 
-    @Test(priority = 3, dependsOnMethods = "myRidesDisplayText")
-    public void activeRidesWindowsScreen() throws Exception {
+    @Test(priority = 3, dependsOnMethods = "verifyMyRidesDisplayText")
+    public void verifyActiveRidesWindowsScreen() throws Exception {
         String activeRidesWindow = myRidesPage.getActiveRidesWindows();
         Assert.assertEquals(activeRidesWindow, "You don't have any active rides");
 
     }
 
-    @Test(priority = 4, dependsOnMethods = "myRidesDisplayText")
-    public void historyRidesTab() throws Exception {
+    @Test(priority = 4, dependsOnMethods = "verifyMyRidesDisplayText")
+    public void verifyHistoryRidesTab() throws Exception {
         myRidesPage.getHistoryRidesTabClick();
         String activeTab = myRidesPage.getHistoryRidesTab();
         Assert.assertEquals(activeTab, "HISTORY");
 
     }
 
-    @Test(priority = 5, dependsOnMethods = "historyRidesTab")
-    public void rideSelectionFromHistory() throws Exception {
+    @Test(priority = 5, dependsOnMethods = "verifyHistoryRidesTab")
+    public void verifyRideSelectionFromHistory() throws Exception {
         myRidesPage.ridesSelectionClick();
         String rideNowButton = myRidesPage.getRideNowButtonText();
         Assert.assertEquals(rideNowButton, "RIDE NOW");
     }
 
-    @Test(priority = 6, dependsOnMethods = "rideSelectionFromHistory")
-    public void getRideNowTitleText() {
+    @Test(priority = 6, dependsOnMethods = "verifyRideSelectionFromHistory")
+    public void verifyGetRideNowTitleText() {
         myRidesPage.getRideNowButtonClick();
         String rideNowTitleText = myRidesPage.getRideNowTitleText();
         Assert.assertEquals(rideNowTitleText, "Select a Timeslot");
         myRidesPage.clickBackButton();
     }
 
-    @Test(priority = 7, dependsOnMethods = "rideSelectionFromHistory")
-    public void selectedHistoryRideTitleText() throws Exception {
+    @Test(priority = 7, dependsOnMethods = "verifyRideSelectionFromHistory")
+    public void verifySelectedHistoryRideTitleText() throws Exception {
         String historyRideTitleText = myRidesPage.getHistoryTitleText();
         Assert.assertEquals(historyRideTitleText, myRidesPage.getRideSelectionDate());
     }
 
-    @Test(priority = 8, dependsOnMethods = "rideSelectionFromHistory")
-    public void pickAndDropLocationText() {
+    @Test(priority = 8, dependsOnMethods = "verifyRideSelectionFromHistory")
+    public void verifyPickAndDropLocationText() {
         String pickupPoint = myRidesPage.getPickupPointText();
         String getPickupPointInMyRidesText = myRidesPage.getPickupPointInMyRidesText();
         if (pickupPoint.equals(getPickupPointInMyRidesText)) {
@@ -93,7 +93,7 @@ public class MyRidesTest extends Setup {
     }
 
     @Test(priority = 10, dependsOnMethods = "verifyNeedHelpWithTheRide", enabled = false)
-    public void getLostAnItemText() {
+    public void verifyGetLostAnItemText() {
 
         myRidesPage.getLostItemClick();
         String getLostItemButtonText = myRidesPage.getLostItemTitleText();
@@ -105,7 +105,7 @@ public class MyRidesTest extends Setup {
     }
 
     @Test(priority = 11, dependsOnMethods = "verifyNeedHelpWithTheRide", enabled = false)
-    public void getPickUpIssue() {
+    public void verifyGetPickUpIssue() {
         myRidesPage.getPickUpItemClick();
         String getPickButtonText = myRidesPage.getPickupPointText();
         Assert.assertEquals(getPickButtonText, "I have an issue with my pickup");
@@ -117,7 +117,7 @@ public class MyRidesTest extends Setup {
     }
 
     @Test(priority = 12, dependsOnMethods = "verifyNeedHelpWithTheRide", enabled = false)
-    public void getDriverIssue() {
+    public void verifyGetDriverIssue() {
         myRidesPage.getIssueWithDriverClick();
         String getDriverButtonText = myRidesPage.getIssueWithDriver();
         Assert.assertEquals(getDriverButtonText, "I have an issue with my driver");
@@ -128,7 +128,7 @@ public class MyRidesTest extends Setup {
     }
 
     @Test(priority = 13, dependsOnMethods = "verifyNeedHelpWithTheRide", enabled = false)
-    public void getOtherIssue() {
+    public void verifyGetOtherIssue() {
         myRidesPage.getOtherIssueClick();
         String getOtherIssueText = myRidesPage.getOtherIssue();
         Assert.assertEquals(getOtherIssueText, "I have some other issue with my trip");
